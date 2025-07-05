0fbf5ef4037b9de0af22fad06212bf3e1a765e32
Oleg Babenko <majerkravchuk@gmail.com>
2025-07-05 19:09:32 +0200

    Add SelectionSort

---
+++ src/Sorting/Implementations/SelectionSort.java
@@ -0,0 +1,44 @@
+package Sorting.Implementations;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class SelectionSort {
+    public static <T extends Comparable<T>> void sort(T[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int m = i;
+            for (int j = i + 1; j < a.length; j++)
+                if (less(a[j], a[m])) m = j;
+            if (m != i) exch(a, i, m);
+        }
+    }
+
+    private static <T extends Comparable<T>> boolean less(T v, T w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static <T extends Comparable<T>> void exch(T[] a, int i, int j) {
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static <T extends Comparable<T>> void show(T[] a) {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static <T extends Comparable<T>> boolean isSorted(T[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (a[i].compareTo(a[i-1]) < 0)
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
